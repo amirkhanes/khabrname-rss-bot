899224ef27eeb98604547affbe3a767cfe921f93
amirkhanes <amirescall@gmail.com>
2026-07-25 21:08:23 +0330

    Initial keywords database

--- keywords.py
+++ keywords.py
@@ -2,7 +2,117 @@ SOURCE_SCORE = 10
 
 MIN_SCORE = 60
 
-COUNTRY_SCORES = {}
-TOPIC_SCORES = {}
-CHANGE_WORDS = {}
-NEGATIVE_WORDS = []
+COUNTRY_SCORES = {
+
+    "iran":30,
+    "iranian":30,
+    "ایران":30,
+    "جمهوری اسلامی":30,
+    "tehran":20,
+    "تهران":20,
+    "israel":20,
+    "اسرائیل":20,
+    "usa":20,
+    "united states":20,
+    "آمریکا":20,
+    "چین":15,
+    "china":15,
+    "روسیه":15,
+    "russia":15
+
+}
+
+TOPIC_SCORES = {
+
+    "تحریم":35,
+    "sanction":35,
+    "برجام":40,
+    "jcpoa":40,
+    "مذاکره":30,
+    "negotiation":30,
+    "آتش بس":40,
+    "ceasefire":40,
+    "جنگ":40,
+    "war":40,
+    "نفت":25,
+    "oil":25,
+    "گاز":25,
+    "gas":25,
+    "دلار":25,
+    "usd":25,
+    "ارز":20,
+    "currency":20,
+    "بانک":20,
+    "bank":20,
+    "بورس":30,
+    "stock":30,
+    "market":25,
+    "اقتصاد":25,
+    "economy":25,
+    "تورم":25,
+    "inflation":25,
+    "بهره":25,
+    "interest rate":25,
+    "مالیات":15,
+    "tax":15,
+    "صادرات":20,
+    "export":20,
+    "واردات":20,
+    "import":20,
+    "پتروشیمی":20,
+    "فولاد":20,
+    "مس":20,
+    "خودرو":20,
+    "بانکی":20,
+    "بیمه":20,
+    "دارویی":20,
+    "غذایی":20,
+    "کشاورزی":20,
+    "حمل و نقل":20,
+    "سیمان":20,
+    "معدن":20
+
+}
+
+CHANGE_WORDS = {
+
+    "price":20,
+    "pricing":20,
+    "قیمت":20,
+    "گرانی":20,
+    "گران":20,
+    "ارزان":20,
+    "ارزانی":20,
+    "cheap":20,
+    "expensive":20,
+    "increase":20,
+    "decrease":20,
+    "growth":20,
+    "drop":20,
+    "rise":20,
+    "fall":20,
+    "افزایش":20,
+    "کاهش":20,
+    "رشد":20,
+    "افت":20,
+    "سقوط":20,
+    "دستمزد":15,
+    "salary":15
+
+}
+
+NEGATIVE_WORDS = [
+    "football",
+    "soccer",
+    "basketball",
+    "tennis",
+    "cricket",
+    "movie",
+    "cinema",
+    "music",
+    "concert",
+    "celebrity",
+    "fashion",
+    "recipe",
+    "cooking"
+]
