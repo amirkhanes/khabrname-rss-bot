b1d7faf83b2cf2c1b5303cd2f32b7866728a3160
amirkhanes <amirescall@gmail.com>
2026-07-25 15:28:01 +0330

    Add EcoNews RSS
    Added a list of RSS feed URLs for various news sources.

---
+++ feeds.py
@@ -0,0 +1,19 @@
+FEEDS = [
+    # Reuters
+    "http://feeds.reuters.com/reuters/topNews",
+
+    # Al Jazeera
+    "https://www.aljazeera.com/xml/rss/all.xml",
+
+    # OilPrice
+    "https://oilprice.com/rss/main",
+
+    # Tasnim
+    "https://www.tasnimnews.com/fa/rss",
+
+    # Mehr
+    "https://www.mehrnews.com/rss",
+
+    # EcoNews
+    "https://www.econews.ir/fa/rss/allnews"
+]
