c220bdb3f0313302f7aa5827caca1eecc3875104
amirkhanes <amirescall@gmail.com>
2026-07-25 21:04:29 +0330

    Connect all modules

--- main.py
+++ main.py
@@ -1,54 +1,32 @@
-import os
 import feedparser
-import requests
-import json
-import time
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHANNEL_ID = "-1004426971236"
+from feeds import FEEDS
+from duplicate_filter import load_sent, is_duplicate, add_news
+from news_filter import filter_news
+from message_builder import build_message
+from telegram_sender import send_message
 
-FEEDS = [
-    "http://feeds.reuters.com/reuters/topNews",
-    "https://www.aljazeera.com/xml/rss/all.xml",
-    "https://oilprice.com/rss/main",
-    "https://www.tasnimnews.com/fa/rss",
-    "https://www.mehrnews.com/rss"
-]
 
-sent_file = "sent.json"
+sent = load_sent()
 
-if os.path.exists(sent_file):
-    with open(sent_file, "r") as f:
-        sent = json.load(f)
-else:
-    sent = []
+for feed in FEEDS:
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(
-        url,
-        data={
-            "chat_id": CHANNEL_ID,
-            "text": text,
-            "disable_web_page_preview": False
-        }
-    )
+    data = feedparser.parse(feed)
 
-while True:
-    for feed in FEEDS:
-        data = feedparser.parse(feed)
+    for item in reversed(data.entries[:10]):
 
-        for item in reversed(data.entries[:5]):
-            link = item.get("link")
+        link = item.get("link", "")
 
-            if link not in sent:
-                title = item.get("title", "")
-                msg = f"📰 {title}\n\n🔗 {link}"
+        if is_duplicate(link, sent):
+            continue
 
-                send_message(msg)
-                sent.append(link)
+        news = filter_news(item, feed)
 
-                with open(sent_file, "w") as f:
-                    json.dump(sent[-200:], f)
+        if news is None:
+            continue
 
-    time.sleep(300)
+        message = build_message(news)
+
+        send_message(message)
+
+        add_news(link, sent)
