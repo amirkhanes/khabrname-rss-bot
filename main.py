e70a0b122582260395dba52b2c9ec50ac3c26e13
amirkhanes <amirescall@gmail.com>
2026-07-27 21:05:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import feedparser
 
 from feeds import FEEDS
@@ -20,53 +19,48 @@ from config import CHECK_COUNT
 
 sent = load_sent()
 
-while True:
+reset_cycle()
 
-    # ریست لیست خبرهای تکراری برای همین اجرای ۵ دقیقه‌ای
-    reset_cycle()
+for source_name, feed in FEEDS.items():
 
-    for source_name, feed in FEEDS.items():
+    try:
 
-        try:
+        data = feedparser.parse(feed)
 
-            data = feedparser.parse(feed)
-
-            if data.bozo:
-
-                if not feed_failed(source_name):
-                    send_warning(source_name, "RSS Parse Error")
-                    mark_failed(source_name)
-
-                continue
-
-            else:
-
-                if feed_failed(source_name):
-                    send_recovered(source_name)
-                    mark_ok(source_name)
-
-            for item in reversed(data.entries[:CHECK_COUNT]):
-
-                link = item.get("link", "")
-
-                if is_duplicate(link, sent):
-                    continue
-
-                news = filter_news(item, source_name)
-
-                if news is None:
-                    continue
-
-                message = build_message(news)
-
-                send_message(message)
-
-                add_news(link, sent)
-
-        except Exception as e:
+        if data.bozo:
 
             if not feed_failed(source_name):
-                send_warning(source_name, str(e))
+                send_warning(source_name, "RSS Parse Error")
                 mark_failed(source_name)
 
-    time.sleep(300)
+            continue
+
+        else:
+
+            if feed_failed(source_name):
+                send_recovered(source_name)
+                mark_ok(source_name)
+
+        for item in reversed(data.entries[:CHECK_COUNT]):
+
+            link = item.get("link", "")
+
+            if is_duplicate(link, sent):
+                continue
+
+            news = filter_news(item, source_name)
+
+            if news is None:
+                continue
+
+            message = build_message(news)
+
+            send_message(message)
+
+            add_news(link, sent)
+
+    except Exception as e:
+
+        if not feed_failed(source_name):
+            send_warning(source_name, str(e))
+            mark_failed(source_name)
